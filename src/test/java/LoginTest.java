f826daaa9eeb2f2f6a53d0dcebe263c220f0619e
Naomi <naomi.bala2003@yahoo.com>
2022-12-13 14:25:51 +0200

    tema lab 17

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,24 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
-
-    public void loginWithValidData(){
+    private  WebDriver driver = new ChromeDriver();
+    @Before
+    public void Driver(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
 
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+
+
+    }
+    @Test
+    public void loginWithValidData(){
+
+
+        driver.findElement(By.cssSelector(".skip-account")).click();
+        driver.findElement(By.cssSelector("[title='Log In']")).click();
         driver.findElement(By.id("email")).sendKeys("cosmin@fasttrackit.org");
         driver.findElement(By.id("pass")).sendKeys("123456");
         driver.findElement(By.id("send2")).click();
 
-        WebElement welcomeTextElement = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div > div.welcome-msg > p.hello > strong"));
+        WebElement welcomeTextElement = driver.findElement(By.cssSelector("p.hello"));
 
         String expectedText = "Hello, Cosmin Fast!";
         String actualText = welcomeTextElement.getText();
@@ -28,6 +36,13 @@ public class LoginTest {
         }else
             System.err.println("Nu s-a logat. ");
 
+        driver.findElement(By.cssSelector(".skip-account")).click();
+        driver.findElement(By.cssSelector("[title='Log Out']")).click();
         driver.close();
+
+
+
+
+
     }
 }
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,27 +1,59 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RegisterTest {
-
-    public void getRegisterTest(){
+     private WebDriver driver = new ChromeDriver();
+    @Before
+    public void Driver() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
+    }
 
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+    @Test
+    public void getRegisterTest(){
+
+        driver.findElement(By.cssSelector(".skip-account")).click();
+        driver.findElement(By.cssSelector("[title=Register]")).click();
         driver.findElement(By.id("firstname")).sendKeys("Austin");
         driver.findElement(By.id("middlename")).sendKeys("Mary");
         driver.findElement(By.id("lastname")).sendKeys("Julya");
-        driver.findElement(By.id("email_address")).sendKeys("tiberia.m@yahoo.com");
+        driver.findElement(By.id("email_address")).sendKeys("austin.mary6@yahoo.com");
         driver.findElement(By.id("password")).sendKeys("acelasinume");
         driver.findElement(By.id("confirmation")).sendKeys("acelasinume");
         driver.findElement(By.cssSelector("#is_subscribed")).click();
-        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button")).click();
 
+        WebElement welcomeText = driver.findElement(By.cssSelector("p.hello"));
+        String expectedText = "Hello, Austin Mary Julya!" ;
+        String actualText = welcomeText.getText();
+        Assert.assertEquals(expectedText,actualText);
+
+        driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > " +
+                "div > div.col-main > div.my-account > div > div:nth-child(4) > " +
+                "div.col2-set > div.col-1 > div > div.box-content > p > a")).click();
+        driver.findElement(By.cssSelector("#current_password")).sendKeys("acelasinume");
+        driver.findElement(By.cssSelector("#password")).sendKeys("acelasinume2");
+        driver.findElement(By.cssSelector("#confirmation")).sendKeys("acelasinume2");
+        driver.findElement(By.cssSelector("[title='Save']")).click();
+
+        WebElement newpassword = driver.findElement(By.cssSelector("body > div > div.page > div.main-container." +
+                "col2-left-layout > div > div.col-main > div.my-account > div > ul > li > ul > li > span"));
+        Assert.assertTrue(newpassword.isDisplayed());
         driver.close();
+
+
+
+
+
+
+
+
 
     }
 }
